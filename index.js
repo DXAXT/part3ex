87e6771b034d04d4f3cc9221eadee172d7e4d02d
DavidTsoiPP <davidt.tdavid@gmail.com>
2025-03-12 17:44:56 +0100

    3.22

--- index.js
+++ index.js
@@ -10,94 +10,93 @@ app.use(morgan('tiny'))
 app.use(express.static('dist'))
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
 }
 
 app.get('/api/persons', (request, response, next) => {
-    Person.find({})
+  Person.find({})
     .then(persons => {
-        response.json(persons)
+      response.json(persons)
     })
     .catch(error => next(error))
 })
 
-app.get('/info', (request,response, next) => {
-    const numberPersons = persons.length
-    response.send(`<div> phonebook has info for ${numberPersons} people </div> <div> ${Date()} </div>`)
-})
+// app.get('/info', (request, response, next) => {
+
+// })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    //const id = Number(request.params.id)
-    // const person = persons.find(person => person.id === request.params.id)
-    // if (person) {
-    //     response.json(person)
-    // } else {
-    //     response.status(404).end()
-    // }
-    Person.findById(request.params.id)
+  //const id = Number(request.params.id)
+  // const person = persons.find(person => person.id === request.params.id)
+  // if (person) {
+  //     response.json(person)
+  // } else {
+  //     response.status(404).end()
+  // }
+  Person.findById(request.params.id)
     .then(person => {
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
 
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
+  Person.findByIdAndDelete(request.params.id)
     .then(result => {
-        response.status(204).end()
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-    
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'content missing or name already added'
-        })
-    }
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
-    // const person = {
-    //     name: body.name,
-    //     number: body.number,
-    //     id: Math.floor(Math.random() * 10000)
-    // }
-
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing or name already added'
     })
+  }
 
-    // persons = persons.concat(person)
-    
-    person.save()
+  // const person = {
+  //     name: body.name,
+  //     number: body.number,
+  //     id: Math.floor(Math.random() * 10000)
+  // }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  // persons = persons.concat(person)
+
+  person.save()
     .then(savedPerson => {
-        response.json(savedPerson)
+      response.json(savedPerson)
     })
     .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
 // this has to be the last loaded middleware, also all the routes should be registered before this!
 app.use(errorHandler)
-  
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -4,7 +4,7 @@ const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery',false)
 
-console.log(`connecting to ${url}`);
+console.log(`connecting to ${url}`)
 mongoose.connect(url)
   .then(result => {
     console.log('connected to MongoDB')
@@ -20,12 +20,12 @@ const personSchema = new mongoose.Schema({
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Person', personSchema)
 
